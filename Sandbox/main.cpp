0377659fa0a3e895f338d87af6664278e1f8cda4
RajShah-94 <rajshah94@gmail.com>
2014-04-12 18:43:18 +0100

    Sandbox

---
+++ Sandbox/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 12 Apr 2014
+ *      Author: rajru_000
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+	do {
+		cin << a << endl;
+	}
+	while(int a%2 = 0);
+}
